21d70a5f196dd03d7b716609a96620debad8d213
Fernando Moreira <fernandoepm.pro@gmail.com>
2023-01-11 00:26:09 -0300

    Adding buttons to details music screen

--- AppMusicBF/MusicDetails/Screen/MusicDetailsScreen.swift
+++ AppMusicBF/MusicDetails/Screen/MusicDetailsScreen.swift
@@ -20,7 +20,7 @@ class MusicDetailsScreen: UIView {
     lazy var view: UIView = {
        let v = UIButton()
         v.translatesAutoresizingMaskIntoConstraints = false
-        v.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.95)
+        v.backgroundColor = UIColor(red: 70/255, green: 50/255, blue: 55/255, alpha: 0.95)
         return v
     }()
 
@@ -53,12 +53,43 @@ class MusicDetailsScreen: UIView {
         return l
     }()
     
+    lazy var musicProgressView: UIProgressView = {
+       let pv = UIProgressView()
+        pv.translatesAutoresizingMaskIntoConstraints = false
+        pv.progress = 0.3
+        pv.backgroundColor = .white
+        pv.progressTintColor = .systemPink
+        return pv
+    }()
+    
     lazy var stackView: UIStackView = {
         let sv = UIStackView()
         sv.translatesAutoresizingMaskIntoConstraints = false
         sv.axis = .horizontal
         sv.distribution = .fillEqually
         return sv
+    }()
+    
+    lazy var goBackButton: UIButton = {
+       let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setImage(UIImage(named: "go_back_music"), for: .normal)
+        return btn
+    }()
+    
+    lazy var playButton: UIButton = {
+       let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setImage(UIImage(named: "play_button"), for: .normal)
+        btn.tintColor = .white
+        return btn
+    }()
+    
+    lazy var goNextButton: UIButton = {
+       let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setImage(UIImage(systemName: "go_next_music"), for: .normal)
+        return btn
     }()
     
     init() {
@@ -78,8 +109,11 @@ class MusicDetailsScreen: UIView {
         self.view.addSubview(self.closeButton)
         self.view.addSubview(self.musicImageView)
         self.view.addSubview(self.musicNameLabel)
+        self.view.addSubview(self.musicProgressView)
         self.view.addSubview(self.stackView)
-        self.stackView.addArrangedSubview(self.notInterestedView)
+        self.stackView.addArrangedSubview(self.goBackButton)
+        self.stackView.addArrangedSubview(self.playButton)
+        self.stackView.addArrangedSubview(self.goNextButton)
     }
     
     private func setupConstraints(){
@@ -96,19 +130,27 @@ class MusicDetailsScreen: UIView {
             self.closeButton.heightAnchor.constraint(equalToConstant: 40),
             self.closeButton.widthAnchor.constraint(equalToConstant: 40),
             
+            self.musicImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200),
             self.musicImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
             self.musicImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50),
             //self.musicImageView.heightAnchor.constraint(equalToConstant: self.musicImageView.frame.width),
             self.musicImageView.heightAnchor.constraint(equalToConstant: 300),
-            self.musicImageView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
             
             self.musicNameLabel.topAnchor.constraint(equalTo: self.musicImageView.bottomAnchor, constant: 20),
             self.musicNameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
             self.musicNameLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30),
             
-            self.stackView.topAnchor.constraint(equalTo: self.musicNameLabel.bottomAnchor, constant: 20),
+            self.musicProgressView.topAnchor.constraint(equalTo: self.musicNameLabel.bottomAnchor, constant: 40),
+            self.musicProgressView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 40),
+            self.musicProgressView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -40),
+            self.musicProgressView.heightAnchor.constraint(equalToConstant: 5),
+            
+            self.stackView.topAnchor.constraint(equalTo: self.musicProgressView.bottomAnchor, constant: 15),
             self.stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
             self.stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30),
+            self.stackView.heightAnchor.constraint(equalToConstant: 70),
+            
+            self.playButton.widthAnchor.constraint(equalToConstant: 50),
 
         ])
         
